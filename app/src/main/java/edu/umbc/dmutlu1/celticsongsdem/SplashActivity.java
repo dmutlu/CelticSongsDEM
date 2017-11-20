95fe82b4212592cb3755d68967408ac0e09591b7
dmutlu <devin.mutlu+github@gmail.com>
2017-11-20 18:34:08 -0500

    Added splashscreen.

--- app/src/main/java/edu/umbc/dmutlu1/celticsongsdem/SplashActivity.java
+++ app/src/main/java/edu/umbc/dmutlu1/celticsongsdem/SplashActivity.java
@@ -14,7 +14,6 @@ public class SplashActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.splash_screen);
         TimerTask task = new TimerTask()
         {
             @Override
